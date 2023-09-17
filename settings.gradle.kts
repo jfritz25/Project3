96237b3be2682fed7ac2e683f3575de6b10e33cb
asteitz <asteitz@iu.edu>
2023-09-17 12:42:35 -0400

    will finish this when i am happ

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,18 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+dependencies {
+    val fragment_version = "1.6.1"
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+}
 
 rootProject.name = "Project3"
 include(":app")
- 
+
+fun dependencies(configuration: DependencyHandlerScope.() -> Unit) {
+
+}
+
+fun implementation(s: String) {
+
+}
