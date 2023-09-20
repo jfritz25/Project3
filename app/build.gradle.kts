33c606bfab973c6b0b99c4f4f85ecb39ed8c037e
asteitz <asteitz@iu.edu>
2023-09-20 00:14:57 -0400

    you could say i ate with this -> idk what i was doing at 8 am also frames are not navigating correctly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.project3"
-    compileSdk = 33
-
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.example.project3"
         minSdk = 24
